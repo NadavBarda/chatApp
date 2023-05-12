de542e4d5c164b89bf5579f4cb53e6dbb82415f0
amirb <amir@cligal.com>
2023-05-12 19:59:44 +0300

    finish

--- ex1b/src/loginScreen/LoginScreen.js
+++ ex1b/src/loginScreen/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./LoginScreen.css";
 
 function LoginScreen() {
@@ -7,21 +7,18 @@ function LoginScreen() {
   const [password, setPassword] = useState("");
   const [formError, setFormError] = useState("");
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+  const navigate = useNavigate();
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
+  const handleLogin = (event) => {
     event.preventDefault();
-    if (!isFormValid()) {
-      setFormError("Please enter a valid username and password.");
-    } else {
+    const storedUser = JSON.parse(localStorage.getItem("registrationData"));
+
+    if (storedUser && username === storedUser.username && password === storedUser.password) {
       setFormError("");
-      // Submit the form
+      localStorage.setItem("userToken", `${storedUser.username}`)
+      navigate("/chat");
+    } else {
+      setFormError("Invalid username or password");
     }
   };
 
@@ -36,46 +33,42 @@ function LoginScreen() {
 
   return (
     <div className="LoginScreen">
-      <br />
-      <p id="logintitle">Log in</p> <br />
-      <form onSubmit={handleSubmit}>
+      <h1 id="logintitle">Login</h1>
+      <form onSubmit={handleLogin} className="form">
         <div className="form-field">
           <label htmlFor="username"></label>
           <input
             type="text"
             id="username"
             name="username"
-            placeholder="Type your user name:"
+            placeholder="Type your user name"
             required
             value={username}
-            onChange={handleUsernameChange}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-        <br />
         <div className="form-field">
           <label htmlFor="password"></label>
           <input
             type="password"
             id="password"
             name="password"
-            placeholder="Enter password:"
+            min={8}
+            placeholder="Enter password"
             required
             pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        {formError && <p className="error-message">{formError}</p>}
-        <br />
-        <br />
         <button id="buttonlogin" type="submit" disabled={!isFormValid()}>
           Login
         </button>{" "}
-        <br />
-        <p>
+        <span>
           Not registered? <Link to="/register">Click here</Link> to register
-        </p>
+        </span>
       </form>
+      {formError && <span className='errorMessage'>{formError}</span>}
     </div>
   );
 }
